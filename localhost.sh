30256fe24fd8c11e9f10cf8b8b1ccd77d1cd8f98
whoguy24 <whoguy24@gmail.com>
2025-10-20 20:13:14 -0500

    Implemented shell script for app launch.

---
+++ localhost.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /Users/whoguy/Library/CloudStorage/Dropbox/OBSS/Projects/PetStar/App
+
+ProcessID=$(lsof -ti:5173)
+if [ -n "$ProcessID" ]; then
+    kill -9 $ProcessID
+fi
+
+npm run dev &
+
+sleep 2
+
+osascript -e 'tell application "Terminal" to set miniaturized of front window to true'
+
+open http://localhost:5173/
