b1dedf401f485d53cd8f1ca99ba6c53e7a87722a
whoguy24 <whoguy24@gmail.com>
2025-10-20 21:58:12 -0500

    Added dumbDog button to refamiliarize myself with React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dumbDog from './assets/golden-retriever-tongue-out.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  var [broke, setBroke] = useState(0)
 
   return (
     <>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Welcome to PetStar!</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <img src={dumbDog} className="logo" alt="Vite logo" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <button onClick={ () => setBroke(1) }>Make the Silly Dog Jump!</button>
+      </div>
+
+      { broke == 1 && 
+        <div>
+          <p>You broke it.</p>
+          <p>Good going, Stupid.</p>
+        </div>
+      }
+
     </>
   )
 }
