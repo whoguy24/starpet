53ebe66585aaeed3e2180f5ae2b2a07e2f050bbe
Warren O'Brien <whoguy24@gmail.com>
2025-10-22 22:07:29 -0500

    Fixed typo in firebase.jsx.

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Authentication and Configuragion
+// Authentication and Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgyc3r9cqOcNx6gmd4hhd67seJehsFq80",
-  authDomain: "starpet-50dcd.firebaseapp.com",
-  projectId: "starpet-50dcd",
-  storageBucket: "starpet-50dcd.firebasestorage.app",
-  messagingSenderId: "774811944915",
-  appId: "1:774811944915:web:1718987a11ed75051ed126"
+  apiKey: process.env.FIRESTORE_API_KEY,
+  authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRESTORE_APP_ID,
 };
 
 // Initialize Firebase
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import { Link } from "react-router-dom"; // if using React Router
-
 const Landing = () => {
   return (
     <div>
