4f5da66031987d00796d3e6e1294b88c2b583cba
whoguy24 <whoguy24@gmail.com>
2025-10-26 11:24:32 -0500

    Refactored user reducer and saga and added comments.

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,8 +1,10 @@
+// Import Modules
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '../firebase/config';
 
+// Request Collection from Firestore
 export async function firestoreFetchUsers() {
-  const usersCol = collection(db, "users");
-  const snapshot = await getDocs(usersCol);
+  const users = collection(db, "users");
+  const snapshot = await getDocs(users);
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-}
+};
--- src/redux/sagas/users.saga.js
+++ src/redux/sagas/users.saga.js
@@ -1,29 +1,21 @@
+// Import Modules
 import { put, takeLatest, call } from 'redux-saga/effects';
-
-// import { select } from 'redux-saga/effects';
-// import { serverStore } from '../reducers/server.reducer.js'
-
-// Define template payload for testing purposes
-
 import { firestoreFetchUsers } from '../../firebase/firestore';
 
+// Fetch Users Collection Request
 function* fetchUsers(action) {
+  try {
+      const users = yield call(firestoreFetchUsers);
+      yield put({ type: 'LOAD_USERS', payload: users });
+  } catch(err) {
+      console.error('ERROR:', err);
+  };
+};
 
-    try {
-
-        const users = yield call(firestoreFetchUsers);
-
-
-        yield put({ type: 'LOAD_USERS', payload: users });
-
-
-    } catch(err) {
-        console.error('ERROR:', err)
-    }
-  }
-
+// Combine Saga Functions
 function* usersSaga() {
   yield takeLatest('FETCH_USERS', fetchUsers);
-}
+};
 
+// Export Module
 export default usersSaga;
