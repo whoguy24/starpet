69aaa280ce3a0b474a48f5bf188f2258794ccfc1
whoguy24 <whoguy24@gmail.com>
2025-11-14 14:54:05 -0600

    Added simple dialog to test.

--- src/components/Animals/AnimalsGallery.jsx
+++ src/components/Animals/AnimalsGallery.jsx
@@ -7,6 +7,14 @@ import { types } from "../../enums/animals/types";
 import { categories } from "../../enums/animals/categories";
 import Card from "../Navigation/Card";
 
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 // Component Function
 function AnimalsGallery() {
     // Define Redux State
@@ -23,35 +31,49 @@ function AnimalsGallery() {
 
     // Define Local State
     const [animalsTable, setAnimalsTable] = useState([]);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         setAnimalsTable(animals.filter((animal) => animal.enum_type === typeKey && animal.enum_category === categoryKey ));
     }, [animals, typeKey]);
 
-    // function debug() {
-    //     dispatch({
-    //         type: "CREATE_ANIMAL",
-    //         payload: {
-    //             contact_ID: "weod4ofF1WzsdChzCPFX",
-    //             name: "Miss Sparky",
-    //             enum_type: "dog",
-    //             enum_category: "toy",
-    //             enum_breed: "pomeranian",
-    //             enum_flag: "red",
-    //             enum_sex: "female",
-    //             enum_size: "extra_small",
-    //             description: "White Pomeranian",
-    //             color: "White",
-    //             skills: {
-    //                 down_stay: false,
-    //                 sit_stay: false,
-    //                 stand_stay: false,
-    //             },
-    //             notes: "Very fussy.",
-    //             active: true,
-    //         },
-    //     });
-    // }
+    function debug() {
+        dispatch({
+            type: "CREATE_ANIMAL",
+            payload: {
+                contact_ID: "weod4ofF1WzsdChzCPFX",
+                name: "Kipling",
+                enum_type: "dog",
+                enum_category: "toy",
+                enum_breed: "australian_terrier",
+                enum_flag: "green",
+                enum_sex: "male",
+                enum_size: "small",
+                description: "Australian Terrier",
+                color: "Brown",
+                skills: {
+                    down_stay: true,
+                    sit_stay: true,
+                    stand_stay: true,
+                },
+                notes: "Good Boy.",
+                active: true,
+            },
+        });
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleClose();
+    };
 
     // Render DOM
     return (
@@ -60,13 +82,55 @@ function AnimalsGallery() {
                 <div className={styles.header}>
                     <h2>{title}</h2>
                 </div>
-                {/* <button onClick={debug}>Add</button> */}
+                <Button onClick={handleClickOpen}>Add a Dog</Button>
                 <div className={styles.links}>
                     {animalsTable.map((animal) => (
                         <Card key={animal.id} path={`/home/animals/${type}/${category}/${animal.id}`} title={animal.name} />
                     ))}
                 </div>
             </div>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add Dog</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="email"
+                        label="Name"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="email"
+                        label="Breed"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="email"
+                        label="Color"
+                        type="email"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={debug}>Add Dog</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
