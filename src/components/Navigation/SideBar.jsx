e3fe4b938724760b63fba1a0235980b4ce600a7d
whoguy24 <whoguy24@gmail.com>
2025-12-02 15:00:22 -0600

    Cleaned up some treeview logic

--- src/components/Navigation/SideBar.jsx
+++ src/components/Navigation/SideBar.jsx
@@ -7,75 +7,72 @@ import { getProjectTypes } from "../../enums/project.types";
 import { SimpleTreeView } from "@mui/x-tree-view/SimpleTreeView";
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
 
+import { useState, useEffect } from "react";
+
+import { useNavigate, useLocation } from "react-router-dom";
+
 import { getRoute } from "../../utils/slugify";
-
-import { useState } from "react";
-
-import { useNavigate } from "react-router-dom";
 
 import styles from "./SideBar.module.css";
 
 function SideBar() {
-    const [treeState, setTreeState] = useState(["animals", "contacts", "organizations", "projects"]);
-
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const [expandedItems, setExpandedItems] = useState([
+        "/home/animals",
+        "/home/contacts",
+        "/home/organizations",
+        "/home/projects",
+    ]);
+    const [selectedItems, setSelectedItems] = useState(["/home"]);
+
+    // useEffect(() => {
+    //     const url = location.pathname;
+    //     const parts = url.split("/").filter(Boolean);
+    //     const ancestors = parts.slice(0, -1).map((_, index) => "/" + parts.slice(0, index + 1).join("/"));
+    //     setExpandedItems(ancestors);
+    // }, [location.pathname]);
+
+    const handleSelectedItemsChange = (event, ids) => {
+        navigate(ids);
+    };
+
+    const handleExpandedItemsChange = (event, itemIds) => {
+        setExpandedItems(itemIds);
+    };
 
     return (
         <div className={styles.container}>
-            <SimpleTreeView defaultExpandedItems={["animals", "contacts", "organizations", "projects"]}>
-                <TreeItem
-                    itemId="home"
-                    label="Home"
-                    onClick={(event) => {
-                        event.stopPropagation();
-                        navigate(`/home`);
-                    }}
-                />
-                <TreeItem
-                    itemId="animals"
-                    label="Animals"
-                    onClick={(event) => {
-                        event.stopPropagation();
-                        navigate(`/home/animals`);
-                    }}
-                >
+            <SimpleTreeView
+                selectedItems={location.pathname}
+                expandedItems={expandedItems}
+                onSelectedItemsChange={handleSelectedItemsChange}
+                onExpandedItemsChange={handleExpandedItemsChange}
+            >
+                <TreeItem itemId="/home" label="Home" />
+                <TreeItem itemId="/home/animals" label="Animals">
                     {getAnimalTypes().map((animalType) => (
                         <TreeItem
                             key={animalType.key}
-                            itemId={animalType.key}
+                            itemId={`/home/animals/${getRoute(animalType.key)}`}
                             label={animalType.plural}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                navigate(`/home/animals/${getRoute(animalType.key)}`);
-                            }}
                         >
                             {getAnimalCategories({ type: animalType.key }).map((animalCategory) => (
                                 <TreeItem
                                     key={animalCategory.key}
-                                    itemId={animalCategory.key}
+                                    itemId={`/home/animals/${getRoute(animalCategory.type)}/${getRoute(
+                                        animalCategory.key,
+                                    )}`}
                                     label={animalCategory.plural}
-                                    onClick={(event) => {
-                                        event.stopPropagation();
-                                        navigate(
-                                            `/home/animals/${getRoute(animalCategory.type)}/${getRoute(
-                                                animalCategory.key,
-                                            )}`,
-                                        );
-                                    }}
                                 >
                                     {getAnimalBreeds({ category: animalCategory.key }).map((animalBreed) => (
                                         <TreeItem
                                             key={animalBreed.key}
-                                            itemId={animalBreed.key}
+                                            itemId={`/home/animals/${getRoute(animalBreed.type)}/${getRoute(
+                                                animalBreed.category,
+                                            )}/${getRoute(animalBreed.key)}`}
                                             label={animalBreed.plural}
-                                            onClick={(event) => {
-                                                event.stopPropagation();
-                                                navigate(
-                                                    `/home/animals/${getRoute(animalBreed.type)}/${getRoute(
-                                                        animalBreed.category,
-                                                    )}/${getRoute(animalBreed.key)}`,
-                                                );
-                                            }}
                                         />
                                     ))}
                                 </TreeItem>
@@ -83,63 +80,30 @@ function SideBar() {
                         </TreeItem>
                     ))}
                 </TreeItem>
-                <TreeItem
-                    itemId="contacts"
-                    label="Contacts"
-                    onClick={(event) => {
-                        event.stopPropagation();
-                        navigate(`/home/contacts`);
-                    }}
-                >
+                <TreeItem itemId="/home/contacts" label="Contacts">
                     {getContactTypes().map((contactType) => (
                         <TreeItem
                             key={contactType.key}
-                            itemId={contactType.key}
+                            itemId={`/home/contacts/${getRoute(contactType.key)}`}
                             label={contactType.plural}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                navigate(`/home/contacts/${getRoute(contactType.key)}`);
-                            }}
                         />
                     ))}
                 </TreeItem>
-                <TreeItem
-                    itemId="organizations"
-                    label="Organizations"
-                    onClick={(event) => {
-                        event.stopPropagation();
-                        navigate(`/home/organizations`);
-                    }}
-                >
+                <TreeItem itemId="/home/organizations" label="Organizations">
                     {getOrganizationTypes().map((organizationType) => (
                         <TreeItem
                             key={organizationType.key}
-                            itemId={organizationType.key}
+                            itemId={`/home/organizations/${getRoute(organizationType.key)}`}
                             label={organizationType.plural}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                navigate(`/home/organizations/${getRoute(organizationType.key)}`);
-                            }}
                         />
                     ))}
                 </TreeItem>
-                <TreeItem
-                    itemId="projects"
-                    label="Projects"
-                    onClick={(event) => {
-                        event.stopPropagation();
-                        navigate(`/home/projects`);
-                    }}
-                >
+                <TreeItem itemId="/home/projects" label="Projects">
                     {getProjectTypes().map((projectType) => (
                         <TreeItem
                             key={projectType.key}
-                            itemId={projectType.key}
+                            itemId={`/home/projects/${getRoute(projectType.key)}`}
                             label={projectType.plural}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                navigate(`/home/projects/${getRoute(projectType.key)}`);
-                            }}
                         />
                     ))}
                 </TreeItem>
