73418cc30dadc5d4d68176679cf2584961ae2b45
whoguy24 <whoguy24@gmail.com>
2025-12-03 11:00:32 -0600

    Reworked our gallery breeds component into new gallery component.

--- src/components/Animals/AnimalsGallery.jsx
+++ src/components/Animals/AnimalsGallery.jsx
@@ -4,11 +4,15 @@ import { getAnimalTypes } from "../../enums/animal.types";
 import { getAnimalCategories } from "../../enums/animal.categories";
 import { getAnimalBreeds } from "../../enums/animal.breeds";
 import Card from "../Navigation/Card";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 import styles from "./AnimalsGallery.module.css";
 
 function AnimalsGallery() {
     const { type, category, breed, id } = useParams();
+
+    const animals = useSelector((state) => state.animals);
 
     let title = "";
     let cardArray = [];
@@ -23,7 +27,7 @@ function AnimalsGallery() {
             image: `/assets/cards/animals/types/${animalType.key}.png`,
         }));
     } else if (!category) {
-        title = getAnimalTypes({ key: getKey(type) }).label;
+        title = getAnimalTypes({ key: getKey(type) }).plural;
         const animalCategories = getAnimalCategories({ type: getKey(type) });
         cardArray = animalCategories.map((category) => ({
             key: category.key,
@@ -32,7 +36,7 @@ function AnimalsGallery() {
             image: `/assets/cards/animals/categories/${category.key}.png`,
         }));
     } else if (!breed) {
-        title = getAnimalBreeds({ key: getKey(breed) }).label;
+        title = getAnimalCategories({ key: getKey(category) }).plural;
         const animalBreeds = getAnimalBreeds({ category: getKey(category) });
         cardArray = animalBreeds.map((animalBreed) => ({
             key: animalBreed.key,
@@ -41,6 +45,22 @@ function AnimalsGallery() {
                 animalBreed.key,
             )}`,
             image: `/assets/cards/animals/breeds/${animalBreed.key}.png`,
+        }));
+    } else if (!id) {
+        title = getAnimalBreeds({ key: getKey(breed) }).plural;
+        const animalsArray = animals.filter(
+            (animal) =>
+                animal.enum_type === getKey(type) &&
+                animal.enum_category === getKey(category) &&
+                animal.enum_breed === getKey(breed),
+        );
+        cardArray = animalsArray.map((animal) => ({
+            key: animal.id,
+            title: animal.name,
+            url: `/home/animals/${getRoute(animal.enum_type)}/${getRoute(animal.enum_category)}/${getRoute(
+                animal.enum_breed,
+            )}/${animal.id}`,
+            image: `/assets/debug/animals/${animal.id}.jpg`,
         }));
     }
 
