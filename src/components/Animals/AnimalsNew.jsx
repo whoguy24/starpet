6b1699e02bd9a53d488b0083dcdfa14704921284
whoguy24 <whoguy24@gmail.com>
2025-12-03 13:44:50 -0600

    Wiring up stepper logic in new animal dialog.

--- src/components/Animals/AnimalsNew.jsx
+++ src/components/Animals/AnimalsNew.jsx
@@ -9,6 +9,7 @@ import Button from "@mui/material/Button";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import Divider from "@mui/material/Divider";
 
 import { useState } from "react";
 
@@ -17,23 +18,59 @@ function AnimalsNew({ open, onClose }) {
 
     const steps = ["Start", "Profile", "Review"];
 
+    function handleSaveButton() {
+        console.log(activeStep);
+    }
+
+    function handleOnClose() {
+        onClose(false);
+        setActiveStep(0);
+    }
+
     return (
-        <Dialog open={open} onClose={() => onClose(false)}>
-            <DialogTitle>New Animal</DialogTitle>
+        <Dialog
+            open={open}
+            onClose={handleOnClose}
+            sx={{
+                "& .MuiPaper-root": {
+                    minWidth: "60%",
+                    minHeight: "60%",
+                },
+            }}
+        >
             <DialogContent>
+                <div className={styles.dialogHeaderContainer}>
+                    <img
+                        className={styles.headerImage}
+                        src={"/assets/cards/home/animals.png"}
+                        onError={(event) => {
+                            event.currentTarget.src = "/assets/cards/placeholder.png";
+                        }}
+                    />
+                    <div className={styles.dialogHeader}>
+                        <div className={styles.dialogHeaderTop}>
+                            <h3 className={styles.dialogHeaderTitle}>Add New Animal</h3>
+                            <Button onClick={handleOnClose}>Cancel</Button>
+                        </div>
+                        <Divider />
+                    </div>
+                </div>
                 <Stepper activeStep={activeStep}>
-                    {steps.map((label, index) => {
-                        return (
-                            <Step key={label}>
-                                <StepLabel>{label}</StepLabel>
-                            </Step>
-                        );
-                    })}
+                    {steps.map((label, index) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
                 </Stepper>
             </DialogContent>
             <DialogActions>
-                {activeStep > 0 && <Button onClick={() => setActiveStep(activeStep - 1)}>Previous</Button>}
-                <Button onClick={() => setActiveStep(activeStep + 1)}>Next</Button>
+                <div className={styles.dialogButtonsContainer}>
+                    <Button onClick={() => setActiveStep(activeStep - 1)}>Previous</Button>
+                    <div>
+                        <Button onClick={() => setActiveStep(activeStep + 1)}>Next</Button>
+                        <Button onClick={handleSaveButton}>Save</Button>
+                    </div>
+                </div>
             </DialogActions>
         </Dialog>
     );
